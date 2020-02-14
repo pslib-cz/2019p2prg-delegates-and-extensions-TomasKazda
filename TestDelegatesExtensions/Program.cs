853c9eae7de097d3a8ae053119fdb563c0f5429f
Tomáš Kazda <36702826+TomasKazda@users.noreply.github.com>
2020-02-14 08:58:06 +0100

    Initialize solution

---
+++ TestDelegatesExtensions/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using TestDelegatesExtensions.Extensions;
+
+namespace TestDelegatesExtensions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICollection<int> empty = new List<int>();
+            ICollection<string> colors = new List<string> 
+            { 
+                "red", "blue", "green", "magenta", "yellow", "cyan", "purple", "orange", "azure", "black", "white","gray","brown","gold", "silver", "red", "yellow", "purple", "green", "green", "red", "red"
+            };
+        }
+    }
+}
---
+++ TestDelegatesExtensions/TestExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestDelegatesExtensions.Extensions
+{
+    public static class TestExtensions
+    {
+        public static Random _rand = new Random();
+
+        // RandomElement
+
+        // RandomElementsWhere
+
+        // AppearanceGreaterThen
+    }
+}
