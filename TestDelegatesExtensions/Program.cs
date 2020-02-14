0fe788ff9e261559e6bddd18c979d0f2015e928f
Tomáš Kazda <tomas.kazda@pslib.cz>
2020-02-14 09:40:06 +0100

    correct solution

--- TestDelegatesExtensions/Program.cs
+++ TestDelegatesExtensions/Program.cs
@@ -13,6 +13,21 @@ namespace TestDelegatesExtensions
             { 
                 "red", "blue", "green", "magenta", "yellow", "cyan", "purple", "orange", "azure", "black", "white","gray","brown","gold", "silver", "red", "yellow", "purple", "green", "green", "red", "red"
             };
+
+            Console.WriteLine(colors.RandomElement());
+            Console.WriteLine(colors.RandomElement());
+
+            Console.WriteLine("dej dva náhodné, krátké:");
+            foreach (var item in colors.RandomElementsWhere(el => el.Length < 4, 2))
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("více než 3×:");
+            foreach (var item in colors.AppearanceGreaterThen(3))
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
--- TestDelegatesExtensions/TestExtensions.cs
+++ TestDelegatesExtensions/TestExtensions.cs
@@ -1,15 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace TestDelegatesExtensions.Extensions
 {
-    class TestExtensions
+    public static class TestExtensions
     {
-        // RandomElement
+        static Random rand = new Random();
 
-        // RandomElementsWhere
+        public static T RandomElement<T>(this IEnumerable<T> collection)
+        {
+            if (collection.Count() > 0)
+                return collection.ElementAt(rand.Next(0, collection.Count()));
+            return default;
+        }
 
-        // AppearanceGreaterThen
+        public static IEnumerable<T> RandomElementsWhere<T>(this IEnumerable<T> collection, Func<T, bool> predicate, int count = 1)
+        {
+            return collection.Where(predicate).OrderBy(itm => rand.NextDouble()).Take(count);
+        }
+
+        public static IEnumerable<T> AppearanceGreaterThen<T>(this IEnumerable<T> collection, int minCount)
+        {
+            var col = collection
+                .GroupBy((itm) => itm, (p, k) => new { item = p, count = k.Count() })
+                .Where((itm) => itm.count >= minCount)
+                .Select(itm => itm.item);
+            return col;
+        }
     }
 }
